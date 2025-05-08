d45e17ad78126124cdfdfefb082f2954d3f46228
AldaXD12 <aldahirchin@gmail.com>
2025-05-08 00:42:40 -0500

    Ajustar configuración de Vite para Netlify y actualizar build

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -5,9 +6,9 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
-    base: env.VITE_BASE_URL,
+    // base: env.VITE_BASE_URL, // Comenta o elimina esta línea
     server: {
-      // Puedes agregar aquí configuraciones específicas del servidor si las necesitas
+      // ...
     },
     build: {
       rollupOptions: {
